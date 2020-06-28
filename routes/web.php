3aa4f0997b9d49750a2f4615fc29dfe4389ec842
jikachuradi <jikachuradi@yahoo.co.jp>
2020-06-28 08:11:27 +0000

    0628メンタリングフィードバック

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('news/create', 'Admin\NewsController@add');
     //課題４．応用
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
 /*「http://XXXXXX.jp/XXX というアクセスが来たときに、 
