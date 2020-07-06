09485504a018c7e58822941f51b6da2c1e29fba0
jikachuradi <jikachuradi@yahoo.co.jp>
2020-07-06 15:53:50 +0000

    PHP/Laravel 16 投稿したニュースを更新/削除しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -27,17 +27,32 @@ public function create(Request $request)
       return redirect('admin/profile/create');
     }
     
-public function edit()
+public function index(Request $request)
 {
-//Profile Modelからデータを取得する
-$profile = Profile::find($request->id);
-if (empty($profile)){
-abort(404);
-}
-return view('admin.news.edit');
+    $cond_title = $request -> cond_title;
+    if ($cond_title != ''){
+        //検索されたら検索結果を取得する
+        $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            //それ以外はすべてのプロフィールを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
 }
 
-public function update()
+//以下を追記
+
+public function edit(Request $request)
+{
+    //News Modelからデータを取得する
+    $profile = Profile::find($request->id);
+    if (empty($profile)){
+        abort(404);
+    }
+    return view('admin.profile.edit',['profile_form' => $profile]);
+}
+
+public function update(Request $request)
 {
     //Validationをかける
     $this->validate($request, Profile::$rules);
@@ -45,11 +60,20 @@ public function update()
     $profile = Profile::find($request->id);
     //送信されてきたフォームデータを格納する
     $profile_form = $request->all();
-    unset($profile_form['_token']);
-   //該当するデータを上書きして保存する
-   $profile->fill($profile_form)->save();
+      unset($profile_form['_token']);
+    //該当するデータを上書きして保存する
+    $profile->fill($profile_form)->save();
+    
+    return redirect('admin/profile/');
+}
 
-return redirect('admin/profile/edit');	
+public function delete(Request $request)
+{
+    //該当するProfile Modelを取得
+    $profile = Profile::find($request->id);
+    //削除する
+    $profile->delete();
+    return redirect('admin/profile/');
 }
 
 }
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -22,33 +22,43 @@
                 <div class="form-group row">
                     <label class="col-md-2" for="name">氏名</label>
                     <div class="col-md-10">
-                    <input type="text" class="form-control" name="name" value="{{ $profile_form->title }}">
+                    <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                    </div>
+                </div>
+                 <div class="form-group row">
+                    <label class="col-md-2" for="gender">性別</label>
+                    <div class="col-md-10">
+                  <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="gender"  value="男性">
+                  <label class="form-check-label">男性</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="gender"  value="女性">
+                  <label class="form-check-label">女性</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="gender"  value="その他">
+                  <label class="form-check-label">その他</label>
+                </div>
+                </div>
+                </div>
+              <div class="form-group row">
+                    <label class="col-md-2">趣味</label>
+                    <div class="col-md-10">
+                        <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-md-2" for="body">本文</label>
+                    <label class="col-md-2">自己紹介欄</label>
                     <div class="col-md-10">
-                    <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-2" for="image">画像</label>
-                    <div class="col-md-10">
-                        <input type="file" class="form-control-file" name="image">
-                        <div class="form-text text-info">
-                            設定中: {{ $news_form->image_path }}
-                        </div>
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除
-                            </label>
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row">
+                    <div class="form-group row">
                     <div class="col-md-10">
-                        <input type="hidden" name="id" value="{{ $news_form->id }}">
+                        <input type="hidden" name="id" value="{{ $profile_form->id }}">
                         {{ csrf_field() }}
-                        <input type="submit" class="btn btn-primary" value="更新">
+                   <input type="submit" class="btn btn-primary" value="更新">
                     </div>
                 </div>
             </form>
